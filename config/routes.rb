8cb1afe61672756c044abdb0e6af0b8f7affe09f
Gwen Au <quyen_au@hotmail.com>
2014-10-23 19:17:39 +0100

    gossip link test works.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ImageSearch2::Application.routes.draw do
+  root :to => 'main#index'
+
+  get '/gossip' => "main#gossip"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ ImageSearch2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'main#index'
+
 
   # See how all your routes lay out with "rake routes"
 
