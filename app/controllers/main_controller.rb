e0b544bc67444eb74faf2329f2bdec0f711fb36c
Gwen Au <quyen_au@hotmail.com>
2014-10-23 18:04:48 +0100

    First commit initializer is working and route is set up.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+
+
+
+  def initialize
+    @colours = ["red", "pink", "orange", "yellow", "green", "blue", "purple"]
+
+    @rwords = ["man", "mountain", "state", "ocean", "country", "building", "cat", "airline", "wealth", "happiness", "pride", "fear", "religion", "bird", "book", "phone", "rice", "snow", "water"]
+  end
+
+  def index
+
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageSearch2::Application.config.secret_token = '86b8f97b6f2ebdf181b08c0919484c43ce30ef4dc64e471168f5b3323344288e0560c1a6eac21ee6fdbe5b3362567bb8b3eb310cef9161b2309e9f6547dddca6'
