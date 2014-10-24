377c23f436250e698785f7ef9b117ea88da97790
Gwen Au <quyen_au@hotmail.com>
2014-10-24 01:24:50 +0100

    All except call to google api working at this stage.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,10 +11,24 @@ class MainController < ApplicationController
   end
 
   def results
-    # binding.pry
     colour = params["format"]
     rword = @rwords.sample
-    @lucky_draw = colour + " & " +rword
+    @lucky_draw = colour + " " + rword
+    
+    entry = @lucky_draw.gsub(' ', '+')
+
+    key = "uwt42R3XI_GyPY3g-r2USfds"
+    id = "215592371804-dbg56b0vg8ocvdoagq28dg66evkn3k0s.apps.googleusercontent.com"
+    # url = "http://imgur.com/search/score?q=#{entry}"
+    # url = "http://www.apimages.com/Search?query=#{entry}&ss=10&st=kw&entitysearch=&toItem=18&orderBy=Newest"
+    # url = "http://www.omdbapi.com/?t=#{entry}"
+    url = "https://www.googleapis.com/customsearch/v1?key="+key+"&cx="+id+"&q="+entry+"&alt=json"
+    # url = "https://www.googleapis.com/customsearch/v1?key=#{key}&cx=#{id}&q=#{entry}&searchType=image&fileType=jpg&imgSize=small&alt=json"
+    binding.pry
+    html = HTTParty.get(url)
+    # HTTParty is the required software.
+    @hash = JSON(html)
+    # @hash = html
   end
 
 end
