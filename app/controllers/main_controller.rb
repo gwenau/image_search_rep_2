38657f81d659d6a0434a23746fca856a8773d3ab
Gwen Au <quyen_au@hotmail.com>
2014-10-24 10:38:39 +0100

    api_key and cx are properly updated in the google http call.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -18,17 +18,21 @@ class MainController < ApplicationController
     entry = @lucky_draw.gsub(' ', '+')
 
     key = "uwt42R3XI_GyPY3g-r2USfds"
-    id = "215592371804-dbg56b0vg8ocvdoagq28dg66evkn3k0s.apps.googleusercontent.com"
+    api_key = "AIzaSyCWS9V5UF4foe0qsutwad_CU8l8w0RvX3c"
+    # id = "215592371804-dbg56b0vg8ocvdoagq28dg66evkn3k0s.apps.googleusercontent.com"
+    id = "003846610913805178311:gydxsvhryt4"
     # url = "http://imgur.com/search/score?q=#{entry}"
     # url = "http://www.apimages.com/Search?query=#{entry}&ss=10&st=kw&entitysearch=&toItem=18&orderBy=Newest"
     # url = "http://www.omdbapi.com/?t=#{entry}"
-    url = "https://www.googleapis.com/customsearch/v1?key="+key+"&cx="+id+"&q="+entry+"&alt=json"
+    url = "https://www.googleapis.com/customsearch/v1?key="+api_key+"&cx="+id+"&q="+entry+"&alt=json"
     # url = "https://www.googleapis.com/customsearch/v1?key=#{key}&cx=#{id}&q=#{entry}&searchType=image&fileType=jpg&imgSize=small&alt=json"
-    binding.pry
+    
     html = HTTParty.get(url)
     # HTTParty is the required software.
     @hash = JSON(html)
     # @hash = html
+    binding.pry
+    # It's important where you put the binding pry, otherwise, it might not come up.
   end
 
 end
