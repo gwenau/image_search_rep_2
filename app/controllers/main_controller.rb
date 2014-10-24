af9b99b2e7d27556a6068c9585c495e934297ca8
Gwen Au <quyen_au@hotmail.com>
2014-10-24 13:16:29 +0100

    Appropriate tests performed on the loop or limit.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,10 +6,6 @@ class MainController < ApplicationController
     @rwords = ["man", "mountain", "state", "ocean", "country", "building", "cat", "airline", "wealth", "happiness", "pride", "fear", "religion", "bird", "book", "phone", "rice", "snow", "water"]
   end
 
-  def index
-
-  end
-
   def results
     colour = params["format"]
     rword = @rwords.sample
